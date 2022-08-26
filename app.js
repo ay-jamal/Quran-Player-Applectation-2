72844b25f9c42a9a1fb0ec0e3e0e450a1de5678d
Ayhem jamal <76484434+ay-jamal@users.noreply.github.com>
2022-08-26 21:53:47 +0200

    push full project to github

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ function getSurah() {
       return response.json();
     })
     .then((data) => {
-      for (let i = 0; i < 115; i++) {
+      for (let i = 0; i < 114; i++) {
         soarhContainer.innerHTML += `
           <div>
-           <p>${data.data[i].asma.ar.long}</p>
-           <p>${data.data[i].asma.en.long}</p>
-           <p>عدد  الآيات :  ${data.data[i].ayahCount} , <span>مكان النزول :  ${data.data[i].type.ar} </span> </p>
+           <p>${data.data[i]?.asma.ar.long}</p>
+           <p>${data.data[i]?.asma.en.long}</p>
+           <p>عدد  الآيات :  ${data.data[i]?.ayahCount} , <span>مكان النزول :  ${data.data[i]?.type.ar} </span> </p>
            
            
            </div>  
@@ -28,13 +28,75 @@ function getSurah() {
       }
 
       let allSurahs = document.querySelectorAll(".surah > div");
-      console.log(allSurahs);
-
+      let ayahAudio;
+      let ayahText;
       allSurahs.forEach((surah, index) => {
         surah.addEventListener("click", () => {
-          let source = document.createElement("source");
-          source.src = `/audio/00${index + 1}.mp3`;
-          audio.appendChild(source);
+          fetch(`https://quran-endpoint.vercel.app/quran/${index + 1}`)
+            .then((response) => response.json())
+            .then((data) => {
+              // console.log(data);
+              let verbes = data.data.ayahs;
+              console.log(verbes);
+              ayahAudio = [];
+              ayahText = [];
+
+              verbes.forEach((verb) => {
+                ayahAudio.push(verb.audio.url);
+                ayahText.push(verb.text.ar);
+              });
+              let ayahIndex = 0;
+              changeAyah(ayahIndex);
+              audio.addEventListener("ended", () => {
+                ayahIndex++;
+                if (ayahIndex < ayahAudio.length) {
+                  changeAyah(ayahIndex);
+                } else {
+                  ayahIndex = 0;
+                  changeAyah(ayahIndex);
+                  audio.pause();
+                  Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "السَّوْرَة اكتملت",
+                    showConfirmButton: false,
+                    timer: 1550,
+                  });
+                }
+              });
+              next.addEventListener("click", () => {
+                ayahIndex < ayahAudio.length - 1 ? ayahIndex++ : ayahIndex == 0;
+                changeAyah(ayahIndex);
+              });
+              prev.addEventListener("click", () => {
+                ayahIndex == 0
+                  ? ayahIndex == ayahAudio.length - 1
+                  : ayahIndex--;
+
+                changeAyah(ayahIndex);
+              });
+              //start button handel
+              let isPlaying = false;
+              tooglePlay();
+
+              function tooglePlay() {
+                if (isPlaying) {
+                  audio.pause();
+                  play.innerHTML = `<i class="fas fa-play"></i>`;
+                  isPlaying = false;
+                } else {
+                  audio.play();
+                  play.innerHTML = `<i class="fas fa-pause"></i>`;
+                  isPlaying = true;
+                }
+              }
+              play.addEventListener("click", tooglePlay);
+
+              function changeAyah(index) {
+                audio.src = ayahAudio[index];
+                ayah.innerHTML = ayahText[index];
+              }
+            });
         });
       });
     });
